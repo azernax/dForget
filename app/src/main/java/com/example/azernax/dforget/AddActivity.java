cf68dd42a22fb3d7880f251b9ebf94b00f0011ef
azernax <mateusz.arc@gmail.com>
2017-03-29 08:10:50 +0200

    create dbHelper class with metods

--- app/src/main/java/com/example/azernax/dforget/AddActivity.java
+++ app/src/main/java/com/example/azernax/dforget/AddActivity.java
@@ -2,8 +2,20 @@ package com.example.azernax.dforget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class AddActivity extends AppCompatActivity {
+
+
+    //
+    private EditText txt_description;
+    private EditText txt_importance;
+    private EditText txt_hour;
+    private EditText txt_minutes;
+    private Button bt_add;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/azernax/dforget/DataBaseHandler.java
+++ app/src/main/java/com/example/azernax/dforget/DataBaseHandler.java
@@ -1,8 +1,50 @@
 package com.example.azernax.dforget;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.ContactsContract;
+
 /**
  * Created by AZERNAX on 29.03.2017.
  */
 
 public class DataBaseHandler {
+
+    //variables
+    DataBaseHelper dbHelper;
+    Context context;
+
+    //constructor
+    public DataBaseHandler(Context ctx)
+    {
+       this.context = ctx;
+        dbHelper = new DataBaseHelper(context);
+    }
+
+
+
+    public static class DataBaseHelper extends SQLiteOpenHelper
+    {
+        //create object to help create, open database etc....
+        public DataBaseHelper(Context ctx)
+        {
+            super(ctx,"myDataBase",null,1);
+        }
+
+        //create db and table if not exist
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL("CREATE TABLE eventsTab(id_event INTEGER PRIMARY KEY AUTOINCREMENT, descrition_event VARCHAR(150) NOT NULL, importance_event VARCHAR(2) DEFAULT 'C',hour_event INTEGER NOT NULL, minutes_event INTEGER NOT NULL, day_event INTEGER NOT NULL, month_event INTEGER NOT NULL, year_event INTEGER NOT NULL);");
+        }
+
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXIST eventsTab");
+            onCreate(db);
+        }
+    }
+
+
 }
