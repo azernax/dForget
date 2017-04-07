c8a21d0a421aa9024bbd34a8f43934cfb6a6230c
azernax <mateusz.arc@gmail.com>
2017-04-07 22:02:39 +0200

    New Design (add TimePicker)

--- app/src/main/java/com/example/azernax/dforget/AddActivity.java
+++ app/src/main/java/com/example/azernax/dforget/AddActivity.java
@@ -1,12 +1,17 @@
 package com.example.azernax.dforget;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.CalendarView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import static com.example.azernax.dforget.R.id.timePicker;
 
 public class AddActivity extends Activity {
 
@@ -14,8 +19,8 @@ public class AddActivity extends Activity {
     //main variables
     private EditText txt_description;
     private EditText txt_importance;
-    private EditText txt_hour;
-    private EditText txt_minutes;
+    private int txt_hour;
+    private int txt_minutes;
     private Button bt_add;
     private int year_get;
     private int month_get;
@@ -24,16 +29,14 @@ public class AddActivity extends Activity {
     DataBaseHandler handler;
 
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
 
-
         txt_description = (EditText) findViewById(R.id.txt_description);
         txt_importance = (EditText) findViewById(R.id.txt_importance);
-        txt_hour = (EditText) findViewById(R.id.txt_hour);
-        txt_minutes = (EditText) findViewById(R.id.txt_minutes);
         bt_add = (Button) findViewById(R.id.bt_add);
 
 
@@ -60,19 +63,21 @@ public class AddActivity extends Activity {
                         //converts data
                         String description = txt_description.getText().toString();
                         String importance = txt_importance.getText().toString();
-                        int hour = Integer.parseInt(txt_hour.getText().toString());
-                        int minutes = Integer.parseInt(txt_minutes.getText().toString());
+
+                        //get time from timePicker
+                        TimePicker timePicker =(TimePicker) findViewById(R.id.timePicker);
+                        txt_hour = timePicker.getHour();
+                        txt_minutes = timePicker.getMinute();
 
                         handler = new DataBaseHandler(getBaseContext()); //create new handler object
                         handler.open(); //open database
-                        handler.InsertData(description, importance, hour, minutes, day_get, month_get, year_get);  //insert data to table
+                        handler.InsertData(description, importance, txt_hour, txt_minutes, day_get, month_get+1, year_get);  //insert data to table
                         handler.close(); //close database
 
                         //clear the EditText fields
                         txt_description.setText("");
                         txt_importance.setText("");
-                        txt_hour.setText("");
-                        txt_minutes.setText("");
+
                     }catch (Exception e)
                     {
                         Toast.makeText(getApplicationContext(),"Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
